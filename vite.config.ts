8da213891ca7c797730dcb74a32b19b1e2310ed1
Szalai Laci <szalai_laci@yahoo.com>
2026-03-21 12:58:48 +0100

    vite config etc

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import mkcert from "vite-plugin-mkcert";
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), mkcert(), tsconfigPaths()],
+  server: {
+    origin: "https://localhost:3000",
+    host: 'localhost',
+    port: 3000,
+  },
+  css: {
+    devSourcemap: true,
+  },
+  build: {
+    sourcemap: true,
+  }
+});
